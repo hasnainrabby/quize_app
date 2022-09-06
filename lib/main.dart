e3508bfb8e85eea9c6316b79ff75256738409ad7
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-09-06 18:03:18 +0600

    Basic Flutter Theme Concept:Adding Custom font

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quize_app/ui/home.dart';
 
 final ThemeData _appTheme = _buildAppTheme();
@@ -11,12 +12,16 @@ final ThemeData _appTheme = _buildAppTheme();
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.blueGrey,
         primaryColor: Colors.black38,
-        textTheme:const TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 30.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.bold),
-        ),
+        textTheme: TextTheme(base.textTheme),
       );
+}
+
+TextTheme(base){
+    return base.copyWith(
+      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+      headline6: TextStyle(fontSize: 30.0, fontStyle: FontStyle.italic),
+      bodyText2: TextStyle(fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.bold,fontFamily: "KaushanScript"),
+    );
 }
 
 void main() => runApp(MaterialApp(
